b06b92b64fe5c63007d131fb5b2905e28a1f18ab
vlaew <vlaew5@gmail.com>
2015-10-19 22:12:34 +0300

    ruby version  aware definition

--- lib/lens/allocations_data.rb
+++ lib/lens/allocations_data.rb
@@ -1,38 +1,49 @@
-require 'objspace'
-
 module Lens
-  module AllocationsData
-    def self.retrieve
-      AllocationsDataMRI.new
+  if RUBY_ENGINE == 'ruby' && RUBY_VERSION >= '1.9'
+    require 'objspace'
+
+    class AllocationsData
+      def initialize
+        @enabled        = false
+        @objects_count  = 0
+        @objects_memory = 0
+      end
+
+      def enable
+        @enabled = true
+      end
+
+      def objects_count
+        @objects_count ||= get_count if @enabled
+        @objects_count
+      end
+
+      def objects_memory
+        @objects_memory ||= get_memory if @enabled
+        @objects_memory
+      end
+
+      private
+
+      def get_count
+        obj_count = ObjectSpace.count_objects
+        obj_count[:TOTAL] - obj_count[:FREE]
+      end
+
+      def get_memory
+        ObjectSpace.count_objects_size[:TOTAL]
+      end
     end
   end
 
-  class AllocationsDataMRI
-    attr_reader :objects_count, :objects_memory
+  unless defined?(::Lens::AllocationsData)
+    class AllocationsData
+      attr_reader :objects_count, :objects_memory
 
-    def initialize
-      @objects_count  = get_count
-      @objects_memory = get_memory
-    end
-
-    private
-
-    def get_count
-      obj_count = ObjectSpace.count_objects
-      obj_count[:TOTAL] - obj_count[:FREE]
-    end
-
-    def get_memory
-      ObjectSpace.count_objects_size[:TOTAL]
-    end
-  end
-
-  class AllocationsDataNone
-    attr_reader :objects_count, :objects_memory
-
-    def initialize
-      @objects_count  = 0
-      @objects_memory = 0
+      def initialize
+        @objects_count  = 0
+        @objects_memory = 0
+      end
     end
   end
 end
