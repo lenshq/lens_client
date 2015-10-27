734cfbbd17fa894b52063529f762136a8fdc0dbe
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-10-27 12:27:15 +0300

    improve notification subscription
    use named variables instead of array index

--- lib/lens/railtie.rb
+++ lib/lens/railtie.rb
@@ -2,18 +2,16 @@ require 'rails'
 
 module Lens
   class Railtie < Rails::Railtie
-    ActiveSupport::Notifications.subscribe(/.*/) do |*args|
-      Trace.process(build_event args)
-    end
-
-    def build_event(args)
-      Event.new(
-        name: args[0],
-        started: args[1],
-        finished: args[2],
-        transaction_id: args[3],
-        payload: args[4]
+    ActiveSupport::Notifications.subscribe(/.*/) do |name, start, finish, id, payload|
+      event = Event.new(
+        name: name,
+        started: start,
+        finished: finish,
+        transaction_id: id,
+        payload: payload
       )
+
+      Trace.process(event)
     end
   end
 end
