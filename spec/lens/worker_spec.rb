a78edcb9ee43590c48554b11c9f1e8392bdfd865
Yana Petrova <janavpetrova@gmail.com>
2015-10-30 23:41:51 +0300

    test worker test worker queue

--- spec/lens/worker_spec.rb
+++ spec/lens/worker_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Lens::Worker do
+  describe '#process' do
+    before { allow(Lens::Sender).to receive :send_to_lens }
 
+    it 'sends data to lens' do
+      expect_any_instance_of(Lens::Sender).to receive :send_to_lens
+      Lens::Worker.new({}).process({})
+    end
+  end
+
+  describe Lens::Worker::Queue do
+    let(:queue) { described_class.new(1) }
+
+    describe '#push' do
+      context 'when queue is full' do
+        before { 2.times { queue.push({}) } }
+        subject { queue.length }
+
+        it { is_expected.to eq 1 }
+      end
+    end
+  end
 end
