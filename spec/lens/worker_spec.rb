a56c81b0524bd8cff46cc26e0b733953b62c6b81
Yana Petrova <janavpetrova@gmail.com>
2015-10-31 00:09:56 +0300

    add tests for shutdown

--- spec/lens/worker_spec.rb
+++ spec/lens/worker_spec.rb
@@ -1,25 +1,50 @@
 require 'spec_helper'
 
 describe Lens::Worker do
+  let(:worker) { Lens::Worker.new({}) }
+
   describe '#process' do
-    before { allow(Lens::Sender).to receive :send_to_lens }
+    before do
+      allow(Lens::Sender).to receive :send_to_lens
+      worker.start
+    end
 
     it 'sends data to lens' do
       expect_any_instance_of(Lens::Sender).to receive :send_to_lens
-      Lens::Worker.new({}).process({})
+      worker.process({})
     end
   end
 
-  describe Lens::Worker::Queue do
-    let(:queue) { described_class.new(1) }
+  describe '#shutdown!' do
+    it 'kills thread thread immediately' do
+      worker.start
+      expect(worker.thread).to be_truthy
 
-    describe '#push' do
-      context 'when queue is full' do
-        before { 2.times { queue.push({}) } }
-        subject { queue.length }
+      worker.shutdown!
+      expect(worker.thread.stop?).to be true
+    end
+  end
 
-        it { is_expected.to eq 1 }
-      end
+  describe '#shutdown' do
+    it 'kills thread thread' do
+      worker.start
+      expect(worker.thread).to be_truthy
+
+      worker.shutdown
+      expect(worker.thread.stop?).to be true
+    end
+  end
+end
+
+describe Lens::Worker::Queue do
+  let(:queue) { described_class.new(1) }
+
+  describe '#push' do
+    context 'when queue is full' do
+      before { 2.times { queue.push({}) } }
+      subject { queue.length }
+
+      it { is_expected.to eq 1 }
     end
   end
 end
