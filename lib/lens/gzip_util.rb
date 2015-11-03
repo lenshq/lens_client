9aabe5a48a761ab1b501872f1f705876acc7db39
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-11-03 13:39:40 +0300

    remove useless method

--- lib/lens/gzip_util.rb
+++ lib/lens/gzip_util.rb
@@ -2,12 +2,6 @@ require 'zlib'
 require 'stringio'
 
 module GzipUtil
-  def self.gunzip(data)
-    io = StringIO.new(data, 'rb')
-    reader = Zlib::GzipReader.new(io)
-    reader.read
-  end
-
   def self.gzip(string)
     io = StringIO.new('w')
     writer = Zlib::GzipWriter.new(io)
